84fb7d5a35aed84f0342924611d829f4405c36c8
Anna Byrd <aibyrd19@gmail.com>
2024-11-04 16:43:26 -0500

    added connectivity distribution histogram

--- tf_knowledge_graph/network_statistics.py
+++ tf_knowledge_graph/network_statistics.py
@@ -3,8 +3,17 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 # load data 
-def load_network_edges(network_type, edge_list_dir="./filtered_edge_list"):
-    network = pd.read_csv(f"{edge_list_dir}/{network_type}/edge_list_filtered.csv", usecols=['source','target','relation'])
+def load_network_edges(edge_list_dir):
+
+    """
+    Loads edges from a CSV where each row contains source,target,[direction or relation]
+    """
+
+    network = pd.read_csv(edge_list_dir, usecols=[0, 1, 2])
+
+    if network.columns[2] == 'direction':
+        network.rename(columns={'direction':'relation'}, inplace=True)
+        
     return {
         'all edges': network, 
         'dn edges': network[network['relation']=='-'], 
@@ -12,7 +21,13 @@ def load_network_edges(network_type, edge_list_dir="./filtered_edge_list"):
         'all nodes': pd.concat([network['source'],network['target']]).unique()
         }
 
+
 def n_nodes(all_edges):
+
+    """"
+    finds the number of unique source and target nodes
+    """
+
     total_srcs = len(set(all_edges['source']))
     total_targs = len(set(all_edges["target"]))
     return {
@@ -20,44 +35,93 @@ def n_nodes(all_edges):
         'targets':total_targs,
     }
 
+
 def avg_node_degree(all_edges, node_type):
+
+    """
+    calculates the average node degree
+    """
+
     return len(all_edges)/len(set(all_edges[node_type]))
 
-# histogram
-def plot_degree_counts_histo(direction, edge_list, fig_size = (10,6), num_bins = 19):
-    if direction == "in":
-        counts = edge_list['target'].value_counts()
-    else:
-        counts = edge_list['source'].value_counts()
 
-    plt.figure(figsize=fig_size)
-    plt.yticks(fontsize=14)
-    plt.xticks(fontsize=14)
-    plt.hist(counts, bins = num_bins, edgecolor ='black')
-    # plt.yscale('log')
-    plt.ylabel('Frequency', fontsize=16)
+def plot_connectivity_distrib(edge_list_file, output, fig_size = (6,5)):
 
-    if direction == "in":
-        plt.xlabel('Target relationships per node', fontsize=16)
-        ticks = np.arange(2, 21, 2)
-        plt.xticks(ticks)
-    else:
-        plt.xlabel('Source relationships per node', fontsize=16)
+    """
+        plots the connectivity distribution using log-scaled histogram of the relative frequency
+        edge_list_file: a file with a list of edges, must have column named 'source' and 'target'
+        output: the directory where to save the images 
+    """
 
-# number of self loops is the number of times the source and target are the same 
+    network = pd.read_csv(edge_list_file, usecols=['source', 'target'])
+
+    # Get the frequency of each node as either a source or a target in an edge
+    out_counts = network['source'].value_counts()
+    in_counts = network['target'].value_counts()
+    out_bins = max(out_counts)//50
+
+    # OUTDEGREE
+    plt.figure(figsize=fig_size, dpi=300)
+    plt.hist(out_counts, bins = out_bins, edgecolor ='none', color='black', 
+             weights = np.ones_like(out_counts) / len(out_counts))
+    plt.yscale('log')
+    
+    plt.ylabel('Relative frequency', fontsize=14)
+    plt.xlabel('Node out-degree', fontsize=14)
+
+    plt.tight_layout()
+    plt.savefig(f"{output}/img/out_degree_unfiltered.png")
+    plt.show()
+
+    # INDEGREE  
+    plt.figure(figsize=fig_size, dpi=300)
+    plt.hist(in_counts, bins = out_bins, edgecolor ='none', color='black', 
+             weights = np.ones_like(in_counts) / len(in_counts))
+    plt.yscale('log')
+    
+    plt.ylabel('Relative frequency', fontsize=14)
+    plt.xlabel('Node in-degree', fontsize=14)
+    plt.xlabel('Node out-degree', fontsize=14)
+
+    plt.tight_layout()
+    plt.savefig(f"{output}/img/in_degree_unfiltered.png")
+    plt.show()
+    
+
+
 def n_self_loops(all_edges):
+
+    """
+    find the number of self loops
+    """
+
     all_edges['self_loop'] = all_edges.apply(lambda row: 1 if row['source'] == row['target'] else 0, axis=1)
     return all_edges['self_loop'].sum()
 
+
 def n_feedback_loops(edge_type):
+
+    """
+    return the number of feedback loops
+    edge_type: specifies positive edges or negative edges
+    """
+
     reversed = edge_type.rename(columns={'source': 'target', 'target': 'source'})
     merged = pd.merge(edge_type, reversed, on=['source', 'relation', 'target'])
     merged = merged[merged['source'] != merged['target']]
     merged['pair'] = merged.apply(lambda row: tuple(sorted((row['source'], row['target']))), axis=1)
     return merged['pair'].drop_duplicates().shape[0]
 
-def all_network_stats(network_type):
-    network = load_network_edges(network_type)
+
+def all_network_stats(edge_list_file, save = False, output = None):
+
+    """
+    calculate all network statistics and print to the screen
+    edge list file: the name of an edge list
+    output: where to optionally save the network stats as a csv
+    """
+
+    network = load_network_edges(edge_list_file)
     all_edges = network['all edges']
     N_nodes = n_nodes(all_edges)
 
@@ -78,6 +142,7 @@ def all_network_stats(network_type):
 
     df = pd.DataFrame([stats])
     print(df.T)
-    
-    # Save as csv - 
-    # df.T.to_csv(f'{output_dir}/{network_type}/network_statistics.csv')
+    if save == True and output != None:
+        df.T.to_csv(f'{edge_list_file}/network_statistics.csv')
+    elif save == True:
+        print("Must provide output directory to save")
